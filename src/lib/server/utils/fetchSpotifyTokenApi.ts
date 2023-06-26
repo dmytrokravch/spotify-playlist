726e4c29d55c61746296772b29cb12def5ada7fa
Dmytro <32672282+dmytrokravch@users.noreply.github.com>
2023-06-26 20:01:56 +0200

    refactor: fetchSpotifyTokenApi

--- src/lib/server/utils/fetchSpotifyTokenApi.ts
+++ src/lib/server/utils/fetchSpotifyTokenApi.ts
@@ -5,49 +5,33 @@ export async function fetchSpotifyTokenApi<T = unknown>(
   fetchOptions?: RequestInit
 ): Promise<FetchApiResult<T>> {
   try {
-    console.log('url', url);
-    console.log('fetchOptions', fetchOptions);
-    const response = await fetch(url, {
-      ...fetchOptions,
-    });
-
-    let body;
-    try {
-      body = await response.json();
-    } catch {
-      body = {};
-    }
+    const response = await fetch(url, fetchOptions);
+    const body = await response.json();
 
     if (!response.ok) {
-      if (body.error?.code && body.error?.message && body.error?.suggestion) {
-        console.error(body.error);
-        return { error: body.error };
-      }
+      const error: ErrorData = body.error?.message
+        ? { ...body.error }
+        : {
+            code: 500,
+            message: `Error when calling ${url}: ${
+              body?.error ?? response.statusText
+            }`,
+            suggestion: 'Please try again',
+          };
 
-      const error = {
-        code: 500,
-        message: `Error when calling ${url}: ${
-          body?.error ?? response.statusText
-        }`,
-        suggestion: 'Please try again',
-      };
       return { error };
     }
 
     return { data: body };
-  } catch (err: any) {
-    if (err?.code && err?.message && err?.suggestion) {
-      console.error(err);
-      return { error: err as ErrorData };
-    }
-
-    const error: ErrorData = {
-      code: 500,
-      message: `Error when calling ${url}`,
-      suggestion: 'Please try again',
-    };
-    console.error(error);
-    console.error(err);
+  } catch (err: unknown) {
+    const error: ErrorData =
+      err instanceof Error
+        ? { ...err }
+        : {
+            code: 500,
+            message: `Error when calling ${url}`,
+            suggestion: 'Please try again',
+          };
     return { error };
   }
 }
